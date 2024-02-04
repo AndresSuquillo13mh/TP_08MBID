c0ba2046efee564e417468fd85894f53ed08427b
AndresSuquillo13mh <jaimhe_13mh@hotmail.com>
2024-02-04 10:33:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TP_08MBID
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/AndresSuquillo13mh/TP_08MBID)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+
+var data = [
+  {"Status":"Good", "count": 6245},
+  {"Status":"Moderate", "count": 4793},
+  {"Status":"Unhealthy for Sensitive Groups", "count": 1246},
+  {"Status":"Unhealthy", "count": 569},
+  {"Status":"Very Unhealthy", "count": 82},
+  {"Status":"Hazardous", "count": 51},
+]
+
+//Gráfico de columnas. Mi 1º gráfico
+var visualization = d3plus.viz()
+  .container("#viz1")
+  .data(data)
+  .order({"value": "count"})
+  .type("bar")
+  .id("Status")
+  .x("Status")
+  .y("count") 
+  .draw()
